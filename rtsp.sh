de7ec49b58563e6f317ff718913b5d9c72474a04
fieldOfView <aldo@fieldofview.com>
2014-09-06 17:10:52 +0200

    Add video bitrate to prevent dropping frames

--- rtsp.sh
+++ rtsp.sh
@@ -1,2 +1,3 @@
 v4l2-ctl --set-ctrl auto_exposure_bias=9
+v4l2-ctl --set-ctrl video_bitrate=2000000
 cvlc v4l2:///dev/video0 --v4l2-width 1080 --v4l2-height 1080 --v4l2-chroma h264 --sout '#rtp{sdp=rtsp://:8554/video.mp4}'
